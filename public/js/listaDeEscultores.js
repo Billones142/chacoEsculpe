fca8df21611ebabf947b3ddfa42e06859c749a5b
Billones142 <stefanomerinoderui@gmail.com>
2024-10-08 02:26:21 -0300

    boton y funcion de votar con la api agregados

--- public/js/listaDeEscultores.js
+++ public/js/listaDeEscultores.js
@@ -1,24 +1,47 @@
-(async () => {
+async function cargarListaDeEscultores(params) {
     listaEscultores= document.getElementById("listaEscultores")
+    listaEscultores.innerHTML = "";
+
     apiResponse= await fetch(origin + "/api/voting")
     listaParaVotar= await apiResponse.json()
+
     listaParaVotar.forEach(element => {
+        const {_id, nombre, votos}= element
+
         divEscultor= document.createElement("div")
         divEscultor.class= "escultor"
-        divEscultor.id= element._id 
+        divEscultor.id= _id
 
         nombreEscultor= document.createElement("a")
-        nombreEscultor.innerHTML="Nombre: " + element.nombre
+        nombreEscultor.innerHTML='<a class="propiedad">Nombre:</a>' + `<a class="valor">${nombre}</a>`
         
         votosEscultor= document.createElement("a")
-        votosEscultor.innerHTML= "Votos: " + element.votos
+        votosEscultor.innerHTML= '<a class="propiedad">Votos:</a>' + `<a class="valor">${votos}</a>`
         
         button= document.createElement("button")
-        button.id= "votar"
+        button.class= "voteButton"
+        button.addEventListener("click", () => {
+            votar(_id, nombre)
+        })
 
         divEscultor.appendChild(nombreEscultor)
         divEscultor.appendChild(votosEscultor)
         divEscultor.appendChild(button)
         listaEscultores.appendChild(divEscultor)
     });
-})()
+}
+
+async function votar(id, nombre) {
+    usuarioVoto= Boolean(localStorage.getItem("usuarioVoto"))
+    if (!usuarioVoto) {
+        await fetch(origin + "/api/voting/" + id,
+            {method: "post"})
+        localStorage.setItem("usuarioVoto", true)
+        alert("Usted votó a " + nombre)
+        cargarListaDeEscultores()
+    } else {
+        alert("Usted ya voto")
+    }
+}
+
+cargarListaDeEscultores()
